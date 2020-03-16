bbe8b79b9ddb509e1d191f5f2372396f5bd9f217
zzy123123 <931728573@qq.com>
2020-03-16 17:23:15 +0800

    增加了底部导航栏，解决了将布局拉伸到整个页面时，底部导航栏被覆盖点不到的问题，然后保留了状态栏

--- app/src/main/java/com/example/garbgeawsl/MainActivity.java
+++ app/src/main/java/com/example/garbgeawsl/MainActivity.java
@@ -1,32 +1,81 @@
 package com.example.garbgeawsl;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
+
 //主界面业务逻辑
 public class MainActivity extends BaseActivity {
+
+    public static boolean hasNavigationBar;
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         hideBottomUIMenu();//隐藏导航栏及状态栏
+
+
+
     }
 
     protected void hideBottomUIMenu(){
         ////隐藏导航栏及状态栏
+
         if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // 这是对于低版本
             View v = this.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
         } else if (Build.VERSION.SDK_INT >= 19) {   //这是实际用的版本哦
             View decorView = getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY ;
             decorView.setSystemUiVisibility(uiOptions);
         }
+
     }
+
+
+
 
 
 }
